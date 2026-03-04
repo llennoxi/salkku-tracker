9e2ae16ecb284f8e54748c34af1f417f94cf6482
llennoxi <simo.punkanen@gmail.com>
2026-03-04 18:18:15 +0200

    Fix ticker map

--- src/App.jsx
+++ src/App.jsx
@@ -17,9 +17,6 @@ const TICKER_MAP = {
   "SOSI1": { yahoo: "SOSI1.HE", currency: "EUR" },
    "LGO": { yahoo: "LGO.TO", currency: "CAD" },
    "FAR.L": { yahoo: "FAR.L", currency: "GBP" },
-  // Lisää tähän tarpeen mukaan:
-  // "NOKIA": { yahoo: "NOKIA.HE", currency: "EUR" },
-  // "SAMPO": { yahoo: "SAMPO.HE", currency: "EUR" },
 };
 
 // Oletusvaluutta tickerille jos ei TICKER_MAP:issa
