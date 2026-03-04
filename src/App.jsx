fb526d557da4b34e2b94adc724d216b326021c7b
llennoxi <simo.punkanen@gmail.com>
2026-03-04 18:12:26 +0200

    Update names and tickers 2

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const USERS = [
 const TICKER_MAP = {
   "SOSI1": { yahoo: "SOSI1.HE", currency: "EUR" },
    "LGO": { yahoo: "LGO.TO", currency: "CAD" },
-   "GPX": { yahoo: "GPX.L", currency: "GBP" },
+   "FAR.L": { yahoo: "FAR.L", currency: "GBX" },
   // Lisää tähän tarpeen mukaan:
   // "NOKIA": { yahoo: "NOKIA.HE", currency: "EUR" },
   // "SAMPO": { yahoo: "SAMPO.HE", currency: "EUR" },
